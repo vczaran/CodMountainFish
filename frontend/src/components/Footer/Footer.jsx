4a67fcbf6d1b35e4aec401e76bd25f588725a77b
Viktoria Czaran <vczaran@gmail.com>
2024-01-22 17:42:24 -0500

    Add subscribe form

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,22 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import Subscribe from "../SubscribeForm/Subscribe";
 
 export default function Footer() {
     return (
-        <footer className="flex justify-between bg-teal-400 absolute inset-x-0 bottom-0 h16">
+        <footer className="flex justify-between bg-teal-400 absolute inset-x-0 bottom-0">
             <img className="object-contain h-20 w-20" src="./logo.png" alt="Logo"></img>
             <Link to="/about">About Us</Link>
             <Link to="/contact">Contact Us</Link>
             <Link to="/directions">
                 <img className="object-contain h-20 w-20" src="./map.png" alt="Map"></img>
             </Link>
-            <div>
-                <h3>Subscribe to our newsletter</h3>
-                <h4>For announcements and exclusive deals</h4>
-                <input type="email"></input>
-            </div>
-
+            <Subscribe />
         </footer>
     )
 }
