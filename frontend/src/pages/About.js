7933aab16425ad363c168d05115496adf408628f
Corbin <corbindbullard@gmail.com>
2024-01-22 17:52:26 -0500

    message

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 export default function About() {
+
+  // THIS IS TO TEST THE API
   const [text, setText] = useState("");
   useEffect(() => {
     fetch("/api/example")
