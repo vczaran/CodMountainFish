e008957e4c2fa80ccfd6d9debcd9a7670c36757d
Viktoria Czaran <vczaran@gmail.com>
2024-01-24 15:14:09 -0500

    Styling fixes

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,22 +7,22 @@ import Subscribe from "../SubscribeForm/Subscribe";
 export default function Footer() {
     return (
         <footer className="flex justify-evenly bg-teal-400 absolute inset-x-0 bottom-0">
-            <img className="object-contain h-20 w-20" src="./logo.png" alt="Logo"></img>
+            <img className="object-contain mt-5 h-20 w-20" src="./logo.png" alt="Logo"></img>
             <Link to="/about">About Us</Link>
             <Link to="/contact">Contact Us</Link>
             <Link to="/directions">
-                <img className="object-contain h-20 w-20" src="./map.png" alt="Map"></img>
+                <img className="mt-5 h-20 w-20" src="./map.png" alt="Map"></img>
             </Link>
             <Subscribe />
             <a
             href="https://www.facebook.com/Codmountain"
-            className="text-s"
+            className="text-lg mt-5"
           >
             <FaFacebookF />
           </a>
           <a
             href="https://www.instagram.com/codmountainfishco"
-            className="text-s"
+            className="text-lg mt-5"
           >
             <FaInstagram />
           </a>
