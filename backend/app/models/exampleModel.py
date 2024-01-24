16d55305c4a02ddb10d4055f190820edffc351e3
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2024-01-24 10:26:13 -0800

    group model init
    rough draft of group model initialized

--- backend/app/models/exampleModel.py
+++ backend/app/models/exampleModel.py
@@ -25,7 +25,7 @@ class Example:
             "id": str(self._id),
             "name": self.name,
             "is_default": self.date,
-            "ownerId": self.isTrue,
+            "isTrue": self.isTrue,
             "created_at": self.created_at,
             "updated_at": self.updated_at
         }
---
+++ backend/app/models/group_Model.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from .db import db
+
+
+class Group:
+
+    collection_name = "Group"
+
+    # Table Columns
+    def __init__(self, user_Id, size_Of_Group, accommodations):
+        self.user_Id = user_Id
+        self.size_Of_Group = size_Of_Group
+        self.accommodations = accommodations
+        # properties only initialized here
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        GroupCollection = db.db[Group.collection_name]
+        result = GroupCollection.insert_one(self.__dict__)
+        return result.inserted_id
+
+    def to_dict(self):
+        return {
+            "id": str(self._id),
+            "user_Id": self.user_Id,
+            "size_Of_Group": self.size_Of_Group,
+            "accommodations": self.accommodations,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
