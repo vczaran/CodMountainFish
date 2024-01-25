9d8734a1785bd2a5ab68afa170f6f0a43f5e6b0a
DingoDang1113 <Shalpeng109@gmail.com>
2024-01-25 15:38:50 -0800

    trip info section

---
+++ frontend/src/components/navbar/BookButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookButton = () => {
+  return (
+  <div className="h-20 w-10 buttonContainer">
+    <button className="h-12 w-20 bg-yellow-600">Book Now! </button>
+  </div>
+  )
+}
+
+export default BookButton;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,36 +1,58 @@
 import React from "react";
 import bg from "../assets/bg.jpg";
+import BookButton from "../components/navbar/BookButton";
 
 export default function Home() {
   return (
-    <div className="h-96  bg-green-600 home">
-    <div className="h-96 bgContainer"   >
-      <div class="h-96 bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }} >  </div>
-    </div>
-
-
-    <div className="h-40 w-90 missionContainer">
-      <div className="h-20 textContainer">
-        <article class="h-30 w-100 prose">
-          <h1> Welcome to Cod Mountain Fish Co.! </h1>
-          <p>We are a premier charter fishing company located in Santa Cruz, California, offering unforgettable guided fishing tours of the beautiful Monterey Bay. Founded by Greg Webb, a dedicated fisherman and seasoned captain, Cod Mountain Fish Co. is committed to providing an exceptional fishing experience for anglers of all skill levels. 
-          </p>
-        </article>
+    <div className="flex flex-col h-full w-full home space-y-0">
+      <div className="relative flex flex-col h-96 bgContainer"   >
+        <div class="h-full w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }} >  
+        </div>
+        
+        <div className="absolute bottom-30 left-0 right-0"> 
+          <BookButton />
+        </div>
       </div>
 
 
-      <div className="h-20 w-10 border-3 border-purple-300 buttonContainer">
-        <button className="h5 w-5 border-3 border-red-600 bg-white-600"></button>
+      <div className="flex flex-col items-center justify-center h-42 w-100 bg-vuejs-400 missionContainer">
+        <div className="flex flex-col items-center justify-center space-y-2 " >
+          {/* <article class="h-30 w-100 prose"> */}
+            <p class="font-bold text-2xl text-white"> Cod Mountain Fish Co.! </p>
+            <p class="text-white">Welcome to Cod Mountain Fish Co., where every fishing trip is tailored to your preferences and skill level. 
+            Our dedicated crew is here to ensure that your time on the water is not only productive but also enjoyable and filled with excitement.
+            </p>
+
+          {/* </article> */}  
+          <BookButton />
+        </div>
+      </div>
+
+      <div> 
+        <p> Trip Types </p>
+
 
       </div>
 
 
+
+      <div> About US (Short)
+
+      </div>
+
+      <div> About Captain (Short)
+
+      </div>
+
+      <div> About Boat (Short)
+
+      </div>
+
+      <div> Customer Reviews (Short)
+
+      </div>
+
     </div>
-
-
-
-
-  </div>
 
 
 
