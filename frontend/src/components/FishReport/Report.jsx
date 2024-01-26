d4080e1bcb62d1a76a29156d1556380c0142cb01
Corbin <corbindbullard@gmail.com>
2024-01-26 15:52:43 -0500

    verticle posts

--- frontend/src/components/FishReport/Report.jsx
+++ frontend/src/components/FishReport/Report.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Report({ image, description, date }) {
   return (
-    <div className="max-w-[80rem] flex min-w-[1080px]">
-      <img className="w-96 rounded-md min-w-96 max-h-[480px]" src={image} alt="fish" />
-      <div className="flex flex-col pl-5 justify-between">
+    <div className="max-w-[80rem] flex flex-col w-[30rem]">
+      <img className="w-[30rem] rounded-md min-w-96 " src={image} alt="fish" />
+      <div className="flex flex-col justify-between pt-4">
+        <p className="italic font-bold mb-2">{date}</p>
         <p>{description}</p>
-        <p className="place-self-end text-gray-500 italic">{date}</p>
       </div>
     </div>
   );
