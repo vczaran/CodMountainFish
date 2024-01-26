d27b52460afab4f1a597734faf5996149677d879
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2024-01-26 11:08:37 -0800

    -- fix dir --

--- frontend/src/pages/FishReport.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function FishReport() {
-  return (
-    <div>
-      Fish Report
-    </div>
-  )
-}
--- frontend/src/pages/TripInfo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TripInfo() {
-  return (
-    <div>
-      Trip Info
-    </div>
-  )
-}
