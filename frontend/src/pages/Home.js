3e3f0baa1c08648c1c26fe2be968d5154eeb95c5
DingoDang1113 <Shalpeng109@gmail.com>
2024-01-29 12:59:53 -0800

    about section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,21 +11,21 @@ export default function Home() {
 
 
   return (
-    <div className="flex flex-col h-full w-full home space-y-0 ">
-      <div className="relative flex flex-col h-96 bgContainer"   >
+    <div className="flex flex-col h-full w-full home">
+      <div className="relative flex flex-col h-72 bgContainer"   >
         <div class="h-full w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }} >  
         </div>
         
-        <div className="absolute bottom-30 left-0 right-0"> 
+        <div className="absolute bottom-20 left-0 right-0"> 
           <BookButton />
         </div>
       </div>
       
-      <div className="flex flex-col items-center justify-center h-30 w-100 bg-vuejs-300 missionContainer">
-        <div className="flex flex-col items-center justify-center space-y-2 " >
-          <article class="w-100 prose">
+      <div className="flex flex-col items-center justify-center h-30 w-full bg-vuejs-300 missionContainer border-4 border-red-400">
+        <div className="flex flex-col items-center justify-center h-26 w-full border-4 border-orange-300" >
+          <article class="flex flex-col h-30 w-full items-center border-4 border-pink-500 prose">
             <p class="font-bold text-2xl text-yellow-500"> Cod Mountain Fish Co.! </p>
-            <p class="text-white">Welcome to Cod Mountain Fish Co., where every fishing trip is tailored to your preferences and skill level. 
+            <p class="text-white mb-2">Welcome to Cod Mountain Fish Co., where every fishing trip is tailored to your preferences and skill level. 
             Our dedicated crew is here to ensure that your time on the water is not only productive but also enjoyable and filled with excitement.
             </p>
 
@@ -34,47 +34,86 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="h-80 flex flex-row items-center justify-around trips"> 
+      <div className="h-96 flex flex-row items-center justify-evenly border-4 border-blue-500 trips"> 
 
         {/* placeholder - will need function to populate info below */}
-        <div className="flex flex-col h-80 w-96 trip mt-4">
-          <div class="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
-            <div class="rounded-md h-full w-full bg-center bg-cover bg-no-repeat" 
+        <div className="flex flex-col h-90 w-96 trip border-4 border-yellow-500">
+          <div class="h-90 bg-green-300 group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
+            <div class="rounded-md h-36 w-full bg-center bg-cover bg-no-repeat" 
                style={{ backgroundImage: `url(${bg})` }} >  
             </div>
             <div className="flex flex-col border-2 rounded-md ">
-              <article className="porse">
-                <h2 className="text-indigo-600 group-hover:text-gray-900 font-bold text-xl">Longrange(Lingcod)</h2>
-                <p className="text-indigo-600 group-hover:text-gray-900 ...">6 hours @ $350.00 </p>
+              <article className="flex flex-col items-center porse">
+                <h2 className="text-indigo-600 group-hover:text-gray-900 font-bold text-xl">Longrange(Lingcod) - 6hr</h2>
+                <p className="text-indigo-600 group-hover:text-gray-900 ...">$350.00/person </p>
                 <p className="text-indigo-500 group-hover:text-gray-500 ...">This charter takes you to the productive waters near Ano Nuevo and Pigeon Point, located north of Santa Cruz. </p>
               </article>
-              <div className="flex flex-row items-start justify-evenly h-14">
+              <div className="flex flex-row items-start justify-evenly h-10">
                 <BookButton />
-                <button className="rounded-md h-10 w-20 bg-yellow-600 text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50"> Details</button> 
+                <button className="rounded-md h-8 w-20 bg-yellow-600 text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50"> Details</button> 
               </div>
             </div> 
           </div>
         </div>
 
 
+        <div className="flex flex-col h-80 w-96 trip border-4 border-yellow-500">
+          <div class="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
+            <div class="rounded-md h-36 w-full bg-center bg-cover bg-no-repeat" 
+               style={{ backgroundImage: `url(${bg})` }} >  
+            </div>
+            <div className="flex flex-col border-2 rounded-md ">
+              <article className="flex flex-col items-center porse">
+                <h2 className="text-indigo-600 group-hover:text-gray-900 font-bold text-xl">Longrange(Lingcod) - 6hr</h2>
+                <p className="text-indigo-600 group-hover:text-gray-900 ...">$350.00/person </p>
+                <p className="text-indigo-500 group-hover:text-gray-500 ...">This charter takes you to the productive waters near Ano Nuevo and Pigeon Point, located north of Santa Cruz. </p>
+              </article>
+              <div className="flex flex-row items-start justify-evenly h-10">
+                <BookButton />
+                <button className="rounded-md h-8 w-20 bg-yellow-600 text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50"> Details</button> 
+              </div>
+            </div> 
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className="h-60 w-full bg-vuejs-200 flex justify-center items-center about" > 
+        <div className="h-52 w-full  bg-blue-200 flex flex-row items-center space-x-2 about-general">
+          <div className="h-48 w-96 bg-purple-200 vidoe" >
+          <div class="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat" 
+               style={{ backgroundImage: `url(${bg})` }} >  
+            </div>
+
+          </div>
+
+          <div className="h-48 w-100">
+              <p className="h-auto w-auto  bg-yellow-300 text">   
+                At Cod Mountain Fish Co., we prioritize sustainability, ethical fishing practices, and responsible charter boat operations. 
+                We firmly believe that protecting our local waters and the fish species we target is of utmost importance. Through our daily operations, we strive to set an example of responsible 
+                practices and educate our customers about the significance of sustainable fishing. We are proud to play a role in preserving our precious marine ecosystem for future generations.
+              </p>
+              <p className="font-bold mt-2"> Read More</p>
+
+          </div>
+
+
+
+
+        </div>
 
       </div>
 
 
 
-      <div> About US (Short)
 
-      </div>
 
-      <div> About Captain (Short)
 
-      </div>
 
-      <div> About Boat (Short)
 
-      </div>
-
-      <div> Customer Reviews
+      <div className="h-40 bg-red-200 " > Customer Reviews
 
       </div>
 
