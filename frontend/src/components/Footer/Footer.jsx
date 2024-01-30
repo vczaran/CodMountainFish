cc1cd18da773e1751252abd836ae3193f9827144
Viktoria Czaran <vczaran@gmail.com>
2024-01-30 14:08:08 -0500

    Change footer map

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,15 +3,23 @@ import { Link } from "react-router-dom";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import Subscribe from "../SubscribeForm/Subscribe";
+import Map from "../Map/Map";
 
 export default function Footer() {
+  const location = {
+    address: 'Cod Mountain Fish Co.',
+    lat: 36.96578633181195,
+    lng: -122.00103392460099,
+  }
+
     return (
         <footer className="flex justify-evenly bg-teal-400 inset-x-0 bottom-0">
             <img className="object-contain mt-5 h-20 w-20" src="./logo.png" alt="Logo"></img>
             <Link to="/about">About Us</Link>
             <Link to="/contact">Contact Us</Link>
             <Link to="/trip-info">
-                <img className="object-contain h-20 w-20" src="./map.png" alt="Map"></img>
+            {/* <Map location={location} zoomLevel={17}/> */}
+            <img className="w-[250px] h-[150px]" src="./map.png" alt="map"></img>
             </Link>
             <Subscribe />
             <a
