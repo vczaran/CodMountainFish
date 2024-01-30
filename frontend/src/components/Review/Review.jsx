94b0cebdb8404e494112162f2a68536ab237b4e5
DingoDang1113 <Shalpeng109@gmail.com>
2024-01-30 14:00:58 -0800

    finish review

--- frontend/src/components/Review/Review.jsx
+++ frontend/src/components/Review/Review.jsx
@@ -1,13 +1,60 @@
 import React from 'react';
 import Slider from "react-slick";
+import { dataReviews } from '../../assets/dataReview';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
 const Review = () => {
+    // const data = dataReviews;
+
+    const settings = {
+        dots: true, 
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024, 
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+
+    };
+
+
+
+
   return (
-    <div>Review</div>
-  )
+    <div className="mx-auto my-5 ">
+        <Slider {...settings}>
+            {dataReviews.map((review, index) => (
+                <div key={index} className="p-2 h-auto min-h-[200px] flex flex-col justify-between overflow-hidden rounded-lg shadow-lg bg-white ">
+                    <div className="mb-2 text-xl font-bold" id="name">{review.name}</div>
+                    <div className="mb-4 text-lg text-yellow-400" id="rating">{'\u2605'.repeat(review.rating)}</div>
+                    <p className="text-sm text-gray-600 " id="description">{review.comments}</p>
+                </div>
+
+            ))}
+        </Slider>
+    </div>
+  );
 }
 
-export default Review
+export default Review;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import bg from "../assets/bg.jpg";
 import fishing from "../assets/fishing.jpg"
 import BookButton from "../components/navbar/BookButton";
+import Review from "../components/Review/Review.jsx";
 
 export default function Home() {
 
@@ -15,7 +16,7 @@ export default function Home() {
     <div className="flex flex-col h-full w-full home">
       {/* Top Background picture - might consider change to carousel */}
       <div className="relative flex flex-col h-72 bgContainer"   >
-        <div class="h-full w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }} >  
+        <div className="h-full w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }} >  
         </div>
         
         <div className="absolute bottom-20 left-0 right-0"> 
@@ -25,9 +26,9 @@ export default function Home() {
       {/* Mission statement section - need to fix the width of inner text */}
       <div className="flex flex-col items-center justify-center h-30 w-full bg-vuejs-300 missionContainer border-4 border-red-400">
         <div className="flex flex-col items-center justify-center h-26 w-full border-4 border-orange-300" >
-          <article class="flex flex-col h-30 w-full items-center border-4 border-pink-500 prose">
-            <p class="font-bold text-2xl text-yellow-500"> Cod Mountain Fish Co.! </p>
-            <p class="text-white mb-2">Welcome to Cod Mountain Fish Co., where every fishing trip is tailored to your preferences and skill level. 
+          <article className="flex flex-col h-30 w-full items-center border-4 border-pink-500 prose">
+            <p className="font-bold text-2xl text-yellow-500"> Cod Mountain Fish Co.! </p>
+            <p className="text-white mb-2">Welcome to Cod Mountain Fish Co., where every fishing trip is tailored to your preferences and skill level. 
             Our dedicated crew is here to ensure that your time on the water is not only productive but also enjoyable and filled with excitement.
             </p>
 
@@ -41,8 +42,8 @@ export default function Home() {
 
         {/* placeholder - will need function to populate info below */}
         <div className="flex flex-col h-90 w-96 trip border-4 border-yellow-500">
-          <div class="h-90 bg-green-300 group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
-            <div class="rounded-md h-36 w-full bg-center bg-cover bg-no-repeat" 
+          <div className="h-90 bg-green-300 group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
+            <div className="rounded-md h-36 w-full bg-center bg-cover bg-no-repeat" 
                style={{ backgroundImage: `url(${bg})` }} >  
             </div>
             <div className="flex flex-col border-2 rounded-md ">
@@ -61,8 +62,8 @@ export default function Home() {
 
 
         <div className="flex flex-col h-80 w-96 trip border-4 border-yellow-500">
-          <div class="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
-            <div class="rounded-md h-36 w-full bg-center bg-cover bg-no-repeat" 
+          <div className="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent">
+            <div className="rounded-md h-36 w-full bg-center bg-cover bg-no-repeat" 
                style={{ backgroundImage: `url(${bg})` }} >  
             </div>
             <div className="flex flex-col border-2 rounded-md ">
@@ -87,7 +88,7 @@ export default function Home() {
       <div className="h-90 w-full flex flex-col justify-center items-center about" > 
         <div className="h-52 w-full p-24 bg-vuejs-300 flex flex-row items-center space-x-4 about-general">
           <div className="h-48 w-96 bg-purple-200 vidoe" >
-          <div class="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat " 
+          <div className="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat " 
                style={{ backgroundImage: `url(${bg})` }} >  
             </div>
 
@@ -117,7 +118,7 @@ export default function Home() {
           </div>
 
           <div className="h-48 w-96 bg-purple-200 vidoe" >
-          <div class="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat " 
+          <div className="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat " 
                style={{ backgroundImage: `url(${bg})` }} >  
             </div>
 
@@ -128,7 +129,7 @@ export default function Home() {
 
         <div className="h-52 w-full p-24 bg-vuejs-300 flex flex-row items-center space-x-4 about-general">
           <div className="h-48 w-96 bg-purple-200 vidoe" >
-          <div class="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat " 
+          <div className="rounded-md h-48 w-96 bg-center bg-cover bg-no-repeat " 
                style={{ backgroundImage: `url(${bg})` }} >  
             </div>
 
@@ -152,29 +153,24 @@ export default function Home() {
       {/* bottom bg */}
 
       <div className="flex flex-col h-80 bgContainer"   >
-        <div class="h-full w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${fishing})` }} >  
+        <div className="h-full w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${fishing})` }} >  
         </div>
       </div>
 
 
       {/* customer review */}
       <div>
-        <h1 className="font-bold text-2xl p-5"> Hear from our awesome users! </h1>
-
+        <h1 className="font-bold text-2xl p-2"> Hear from our awesome users! </h1>
 
       </div>
 
-      <div class="flex flex-row items-center justify-between space-x-2 max-w-sm mx-auto bg-white rounded-lg overflow-hidden shadow-lg my-5 ">
-        <div class="p-5 border-2">
-            <div class="font-bold text-xl mb-2" id="name">Shali P.</div>
-            <div class="rating mb-4 text-yellow-400" id="rating">★★★★★</div>
-            <p class="text-gray-600 text-sm" id="description">Nostrud aute id elit proident veniam ex. Elit enim laborum enim velit laborum anim. Occaecat do non velit sint elit mollit. </p>
-        </div>
+      <div className="p-4 ">
+        <Review />
+
+      </div>
 
 
 
-       
-    </div>
 
 
 
