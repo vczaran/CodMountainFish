4c94eff7b287b157572c7915dbdd1e706c5c1ff3
Viktoria Czaran <vczaran@gmail.com>
2024-01-30 14:44:32 -0500

    Add directions

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import Subscribe from "../SubscribeForm/Subscribe";
-import Map from "../Map/Map";
+// import Map from "../Map/Map";
 
 export default function Footer() {
-  const location = {
-    address: 'Cod Mountain Fish Co.',
-    lat: 36.96578633181195,
-    lng: -122.00103392460099,
-  }
+  // const location = {
+  //   address: 'Cod Mountain Fish Co.',
+  //   lat: 36.96578633181195,
+  //   lng: -122.00103392460099,
+  // }
 
     return (
         <footer className="flex justify-evenly bg-teal-400 inset-x-0 bottom-0">
--- frontend/src/pages/TripInfo.js
+++ frontend/src/pages/TripInfo.js
@@ -3,7 +3,45 @@ import React from 'react'
 export default function TripInfo() {
   return (
     <div>
-      Trip Info
+      <h1>Directions</h1>
+      <h3>From Highway 17:</h3>
+      <ol className="list-decimal ml-10">
+        <li>Take the exit onto CA-1 S toward Watsonville/Monterey.</li>
+        <li>Continue on CA-1 S for approximately 2 miles.</li>
+        <li>Take the Soquel Ave exit.</li>
+        <li>Keep right at the fork and merge onto Soquel Ave.</li>
+        <li>Continue on Soquel Ave for approximately 0.3 miles.</li>
+        <li>Turn left onto Seventh Ave.</li>
+        <li>Follow Seventh Ave for approximately 0.4 miles.</li>
+        <li>Turn right onto Eaton St.</li>
+        <li>Continue on Eaton St for approximately 0.2 miles.</li>
+        <li>Turn left onto Lake Ave.</li>
+        <li>Take the first right into Santa Cruz Harbor.</li>
+        <li>Park at a metered spot and purchase a parking ticket from the pay kiosk.</li>
+        <li>Proceed to "S" dock.</li>
+      </ol>
+      <h3>From Highway 1 Heading North:</h3>
+      <ol className="list-decimal ml-10">
+        <li>Continue on CA-1 N.</li>
+        <li>Take the Soquel Dr exit toward Porter St.</li>
+        <li>Use the left two lanes to turn left onto Soquel Dr.</li>
+        <li>Continue on Soquel Dr for approximately 0.8 miles.</li>
+        <li>Turn right onto Seventh Ave.</li>
+        <li>Follow Seventh Ave for approximately 0.4 miles.</li>
+        <li>Turn right onto Eaton St.</li>
+        <li>Continue on Eaton St for approximately 0.2 miles.</li>
+        <li>Turn left onto Lake Ave.</li>
+        <li>Take the first right into Santa Cruz Harbor.</li>
+        <li>Park at a metered spot and purchase a parking ticket from the pay kiosk.</li>
+        <li>Proceed to "S" dock.</li>
+      </ol>
+      {/* Map */}
+      {/* What to Wear */}
+      {/* What's Included? */}
+      {/* Link to get Fishing License */}
+      {/* https://wildlife.ca.gov/Licensing/Fishing */}
+      {/* FAQs */}
+      {/* Cancellation Policy */}
     </div>
   )
 }
