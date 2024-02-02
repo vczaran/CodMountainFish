c1d38c18417fa0810c52699181d5417dd5374ad3
Viktoria Czaran <vczaran@gmail.com>
2024-02-02 17:07:13 -0500

    Add faq page

--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -2,6 +2,46 @@ import React from "react";
 
 export default function FAQ () {
     return (
-        <div>Hi from FAQ!</div>
+        <div className="space-y-10 mb-20">
+            <h1 className="text-xl font-bold text-center">Frequently Asked Questions</h1>
+            <ol className="list-decimal space-y-10 ml-10">
+                <li>What types of trips do you offer? <br></br>
+                We offer guided fishing tours in the Monterey Bay, catering to both recreational and experienced anglers. Our trips focus on West Coast ground fish species such as rockfish, 
+                lingcod, and halibut. We also offer seasonal trips targeting bluefin tuna during late summer and early fall. We also offer whale watching tours, at sea burials, and are a general 
+                boat for hire for scientific research.</li>
+                <li>Do I need fishing experience to join a trip? <br></br>
+                No prior fishing experience is required. Our fishing trips are suitable for anglers of all skill levels, from beginners to seasoned pros. Our crew will provide guidance and 
+                assistance throughout the trip to ensure a successful and enjoyable fishing experience.</li>
+                <li> What should I bring on the fishing trip? <br></br>
+                We recommend bringing sunscreen, sunglasses, a hat, layered clothing, and comfortable shoes with non-slip soles. You are welcome to bring your own fishing gear, but we provide 
+                all necessary tackle and gear by top industry brands. You may bring an ice chest, but there is no need for one on the boat, however it will be good to have one to take your fish 
+                home in. Don't forget to bring your excitement and enthusiasm for a day of fishing!</li>
+                <li>Can I bring my own food and drinks? <br></br>
+                Absolutely! You are welcome to bring your own food and beverages. We have ample cold storage space on board to keep your drinks and snacks fresh throughout the trip. 
+                However, please note that alcohol is not permitted on our fishing trips.</li>
+                <li>What happens if the weather is bad? <br></br>
+                The safety and comfort of our passengers are our top priorities. In the event of inclement weather, our experienced captain will assess the conditions and make a decision 
+                regarding the trip. If a trip needs to be canceled or rescheduled due to weather, we will notify you as soon as possible and provide alternative options including a full refund.</li>
+                <li>How many people can join a fishing trip? <br></br>
+                Our fishing trips can accommodate up to 4 passengers. Whether you're planning a solo adventure, a family outing, or a group excursion, we can tailor the trip to your specific 
+                needs and ensure everyone has a great time on board.</li>
+                <li>Is gratuity included in the trip cost? <br></br>
+                No, gratuity is not included in the trip cost. If you feel that our crew has provided exceptional service, it is customary to offer a gratuity as a token of appreciation. 
+                The amount is entirely at your discretion.</li>
+                <li>How do I book a fishing trip? <br></br>
+                Booking a fishing trip with us is easy. Simply visit our website and navigate to the "Book Now" page, where you can select your preferred date and trip option. Follow the prompts to 
+                complete the booking process, and we will confirm your reservation shortly after.</li>
+                <li>Can I reschedule or cancel my booking? <br></br>
+                We understand that plans may change. If you need to reschedule or cancel your booking, please notify us at least 48 hours in advance, and we will do our best to accommodate your request. 
+                Please refer to our booking policy for more information.</li>
+                <li>How can I contact Cod Mountain Fish Co. for further inquiries? <br></br>
+                We are here to assist you with any additional questions or concerns. You can reach us by phone at 1-831-419-8443, or you can send us an email at codmountainfishco@gmail.com. 
+                Our friendly team will be happy to provide you with the information you need.</li>
+                <li>Can I play my own music? <br></br>
+                Yes, we can connect to your device through Bluetooth or by USB, and play your tunes all day.</li>
+                <li>Will there be time to fish for halibut when we are done catching all our Rockfish? <br></br>
+                No, but you have the option of booking a halibut only trip, provided they are in season and available.</li>
+            </ol>
+        </div>
     )
 }
