f43f960f65c1ea8227a948bb77f46ba538cf5e0c
lola <keepthechange2021@gmail.com>
2024-02-01 16:04:06 -0800

    working on details side box

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -54,31 +54,31 @@ const Calendar = () => {
     console.log("currentDayBookings", currentDayBookings)
 
     const remainingSeats = () => {
-    const totalSeats = 6;
-    let remainingSeatsAM = totalSeats;
-    let remainingSeatsPM = totalSeats;
+        const totalSeats = 6;
+        let remainingSeatsAM = totalSeats;
+        let remainingSeatsPM = totalSeats;
 
-    currentDayBookings.forEach(booking => {
-        if (booking.tripType === 'tuna') {
-            remainingSeatsAM = 0;
-            remainingSeatsPM = 0;
-        } else if (booking.time === 'am') {
-            remainingSeatsAM -= booking.partySize;
-        } else if (booking.time === 'pm') {
-            remainingSeatsPM -= booking.partySize;
-        }
-    });
+        currentDayBookings.forEach(booking => {
+            if (booking.tripType === 'tuna') {
+                remainingSeatsAM = 0;
+                remainingSeatsPM = 0;
+            } else if (booking.time === 'am') {
+                remainingSeatsAM -= booking.partySize;
+            } else if (booking.time === 'pm') {
+                remainingSeatsPM -= booking.partySize;
+            }
+        });
 
-    return { remainingSeatsAM, remainingSeatsPM };
+        return { remainingSeatsAM, remainingSeatsPM };
 
-}
+    }
     console.log("remainingSeats", remainingSeats())
 
     return (
         <div>
 
             <div className="flex justify-center gap-5 items-center">
-                <div className="calendar-box w-[710px] h-[400px]">
+                <div className="calendar-box w-[600px] h-[400px]">
                     <div className="flex gap-5 pb-2">
                         <div className="font-semibold">{months[today.month()]}, {today.year()}</div>
                         <div className="flex items-center gap-5">
@@ -155,11 +155,56 @@ const Calendar = () => {
                     </div>
                 </div>
 
-                <div className="side-details-box w-[300px] h-[400px] border p-5 mt-[110px]">
+                {/* <div className="side-details-box w-[300px] h-[400px] border p-5 mt-[110px]">
                     <div className="font-semibold">
                         {selectedDate.toDate().toDateString()}
                     </div>
                     <p>Other info trip info????</p>
+                </div> */}
+                <div className="side-details-box w-[310px] h-[400px] border p-5 mt-[110px]">
+                    <div className="font-semibold">
+                        {selectedDate.toDate().toDateString()}
+                    </div>
+                    {currentDayBookings.some(booking => booking.tripType === 'tuna') ? (
+                        <>
+                            <div className="flex">
+                            <img src="./tuna.jpeg" alt="tuna" className="h-10 w-10 object-contain"/>
+                            <p>Tuna Trip</p>
+                            </div>
+                            <div className="flex">
+                            <button className="bg-gray-200 rounded-full px-2 py-1 inline-block">6 a.m.</button>
+                            <p>(0 seats available)</p>
+                            </div>
+                            <div className="bg-gray-200 rounded-full px-2 py-1 inline-block">full boat</div>
+                        </>
+                    ) : currentDayBookings.length === 0 ? (
+                        <>
+                        <div className="border rounded-md p-2">
+                            <div className="flex items-center gap-2">
+                            <img src="./rockfish.png" alt="rockfish" className="h-10 w-10 object-contain"/>
+                            <p>Rockfish Trip</p>
+                            </div>
+
+                            <div className="flex items-center gap-2 mb-1">
+                            <div className="border rounded-md px-2 py-1 inline-block ml-5">6 a.m.</div>
+                            <button className="rounded-md px-3 py-1 inline-block bg-amber-400 text-white ">Book</button>
+                            <p className="text-xs">(6 seats left!)</p>
+                            </div>
+
+                            <div className="flex items-center gap-2">
+                            <button className="border rounded-md px-2 py-1 inline-block ml-5">2 p.m.</button>
+                            <button className="rounded-md px-3 py-1 inline-block bg-amber-400 text-white ">Book</button>
+                            <p className="text-xs">(6 seats left!)</p>
+                            </div>
+                        </div>
+                            <img src="./halibut.jpeg" alt="halibut" />
+                            <p>Halibut Trip</p>
+                            {/* Add similar buttons and text for the Halibut and Wildlife trips */}
+                        </>
+                    ) : (
+                        <></>
+                        // Add your condition and rendering for a trip with a morning charter here
+                    )}
                 </div>
             </div>
         </div>
