07c826ee95e10cce5aad9df9d81305ae40f13633
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2024-02-02 09:51:59 -0800

    deleted example

--- backend/app/forms/example_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import (StringField, DateTimeField, BooleanField)
-from wtforms.validators import DataRequired
-
-
-class ExampleForm(FlaskForm):
-    name = StringField("Name")
-    date = DateTimeField("Date")
-    isTrue = BooleanField("is it True")
