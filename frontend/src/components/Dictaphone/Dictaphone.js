1b43fb4b263391d1eeeff7848e45a6f5ab264f13
Viktoria Czaran <vczaran@gmail.com>
2024-02-03 16:28:12 -0500

    Add dictaphone styling

--- frontend/src/components/Dictaphone/Dictaphone.js
+++ frontend/src/components/Dictaphone/Dictaphone.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { FaMicrophone } from "react-icons/fa";
 
 const Dictaphone = () => {
     const {
@@ -14,11 +15,11 @@ const Dictaphone = () => {
     }
   
     return (
-      <div className='text-right mr-10'>
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
+      <div className='text-right mr-10 space-y-1 space-x-2'>
+        <p> <FaMicrophone className='text-2xl absolute right-20'/> {listening ? 'On' : 'Off'}</p>
+        <button className="border-solid border-2 border-black rounded-sm p-1 hover:underline" onClick={SpeechRecognition.startListening}>Start</button>
+        <button className="border-solid border-2 border-black rounded-sm p-1 hover:underline" onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button className="border-solid border-2 border-black rounded-sm p-1 hover:underline" onClick={resetTranscript}>Reset</button>
         <p>{transcript}</p>
       </div>
     );
